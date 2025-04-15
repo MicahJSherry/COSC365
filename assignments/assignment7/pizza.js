552286b861af667cb76b8f9e231cbcf508aa7dae
Micah Sherry <MicahJSherry@gmail.com>
2025-04-15 09:38:50 -0400

    added minor touches to assignment 7

--- assignments/assignment7/pizza.js
+++ assignments/assignment7/pizza.js
@@ -21,11 +21,15 @@ function calculateTotal() {
     var price = 9.90;
 
     var XLarge = document.getElementById("XLarge");
-
-
+    var Large = document.getElementById("Large");
     if (XLarge.checked) {
-
         price += 3.05;
+        XLarge.parentNode.style.backgroundColor = "aqua";
+        Large.parentNode.style.backgroundColor  = "white";
+    
+    } else { // large is checked by default
+        XLarge.parentNode.style.backgroundColor = "white";
+        Large.parentNode.style.backgroundColor  = "aqua";
     }
 
     var toppings = document.getElementById("toppings");
@@ -37,21 +41,24 @@ function calculateTotal() {
         if (t[i].checked) {
             price += 1.25;
             hasTopping = true;
+            t[i].parentNode.style.backgroundColor = "yellow";
+        } else {
+            t[i].parentNode.style.backgroundColor = "white";
         }
     }
 
     if (hasTopping) {
-        var pizzaImg = document.getElementById("pizzaImg").src = "images/pizza-pepperoni.png"
+        var pizzaImg = document.getElementById("pizzaImg").src = "images/pizza-pepperoni.png";
 
     } else {
-        var pizzaImg = document.getElementById("pizzaImg").src = "images/pizza-cheese.png"
+        var pizzaImg = document.getElementById("pizzaImg").src = "images/pizza-cheese.png";
     }
 
     var crust = document.getElementById("crust");
     if (crust.value == "thin") {
         price += 1;
     } else if (crust.value == "deep") {
-        price += 2
+        price += 2;
     }
 
     var total = document.getElementById("total");
